6db5f0ab9b10f18ef4222cb6fde9e7d73a43d488
emil6957 <emiliano.milewski@gmail.com>
2024-04-13 13:35:55 +0100

    added post logic for the form to be able to add new message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+    res.render("index", { title: "Message Board", messages: messages });
+});
+
+router.get("/new", function(req, res, next) {
+    res.render("form", { messages: messages });
+});
+
+router.post("/new", function(req, res, next) {
+    const message = req.body.message;
+    const user = req.body.user;
+    messages.push({ text: message, user: user, added: new Date() });
+    res.redirect("/");
 });
 
 module.exports = router;
